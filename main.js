509515631abc4420e4a85efdb491256cb385af5b
Jason Tomsic <jasontomsic@quickenloans.com>
2023-11-27 11:48:19 -0500

    page layout

---
+++ main.js
@@ -0,0 +1,42 @@
+let commando = {
+    name: 'Commando',
+    color: '#ED9616',
+    subdir: 'commando',
+    numSkins: '7',
+    primary: [
+        {
+            slot: 'main',
+            name: 'Double Tap (main)',
+        },
+    ],
+    secondary: [
+        {
+            slot: 'main',
+            name: 'Full Metal Jacket (main)',
+        },
+        {
+            slot: 'alt',
+            name: 'Combat Knife (alt)',
+        }
+    ],
+    utility: [
+        {
+            slot: 'main',
+            name: 'Tactical Dive (main)',
+        },
+        {
+            slot: 'alt',
+            name: 'Tactical Slide (alt)',
+        }
+    ],
+    special: [
+        {
+            slot: 'main',
+            name: 'Suppressive Fire (main)',
+        },
+        {
+            slot: 'alt',
+            name: 'Point-Blank (alt)',
+        }
+    ],
+};
