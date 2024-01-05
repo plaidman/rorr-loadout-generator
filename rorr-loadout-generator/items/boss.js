0a278f26c3b805c2c182528a499b4a6411c5c069
Jason Tomsic <jasontomsic@quickenloans.com>
2024-01-05 18:36:43 -0500

    added images and data arrays for items

---
+++ rorr-loadout-generator/items/boss.js
@@ -0,0 +1,44 @@
+export const bossItems = [
+    {
+        name: "Legendary Spark",
+        healing: false,
+        utility: false,
+        damage: false,
+        image: "boss/Legendaryspark.webp",
+    },
+    {
+        name: "Imp Overlord's Tentacle",
+        healing: false,
+        utility: false,
+        damage: false,
+        image: "boss/Impoverlordstentacle.webp",
+    },
+    {
+        name: "Burning Witness",
+        healing: false,
+        utility: false,
+        damage: false,
+        image: "boss/Burningwitness.webp",
+    },
+    {
+        name: "Colossal Knurl",
+        healing: false,
+        utility: false,
+        damage: false,
+        image: "boss/Colossalknurl.webp",
+    },
+    {
+        name: "Ifrit's Horn",
+        healing: false,
+        utility: false,
+        damage: false,
+        image: "boss/Ifritshorn.webp",
+    },
+    {
+        name: "Scorching Shell Piece",
+        healing: false,
+        utility: false,
+        damage: false,
+        image: "boss/Scorchingshellpiece.webp",
+    },
+];
---
+++ rorr-loadout-generator/items/bossequip.js
@@ -0,0 +1,9 @@
+export const bossEquipItems = [
+    {
+        name: "Nematocyst Nozzle",
+        healing: false,
+        utility: false,
+        damage: false,
+        image: "boss/Nematocystnozzle.webp",
+    },
+];
